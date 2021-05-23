cdf406f2e55623c09e0745083da3cf8809e6478c
gkswn911 <gkswn911@naver.com>
2021-05-23 22:06:07 +0900

    Edit : Delete airdata and keys
    Add : movie.js

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', function(req,res,next){
-    res.send(req.body.title);
-    console.log(req.body.title);
+
+router.post('/', function(req,res){
+
+  movieSearch(req.body.title, {air}={})=>{
+    if (error){
+      return res.send({error})
+    }
+    return res.render('search', {
+      이름 : '이한주',
+      title: air['parm'['title']]
+
+    })
+  }
 });
 
 
+
+
+
 module.exports = router;
+
