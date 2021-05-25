9edc7245979ab027126702c35701c7ea4f1302d1
gkswn911 <gkswn911@naver.com>
2021-05-25 18:46:09 +0900

    Add : Add FitMoive feature
    and edit some files

--- routes/search.js
+++ routes/search.js
@@ -5,22 +5,22 @@ const alert = require('alert');
 
 router.get('/', function(req,res,next){
   
-  movieSearch(req.query.title,(error, {air}={})=>{
+  movieSearch(req.query.title,(error, {movie}={})=>{
     
     if (req.query.title == ""){
       alert("검색할 영화 제목을 입력해주세요.");
       return res.render('index');
     }
-    else if( air['total'] == 0 ){
+    else if( movie['total'] == 0 ){
       alert("결과가 없습니다. 다른 제목을 입력해주세요.");
       return res.render('index');
     }
 
     for(let i = 0; i < 5; i++){
-    air['items'][0]['title'] = air['items'][0]['title'].replace(/<b>/,"")
-    air['items'][0]['title'] = air['items'][0]['title'].replace(/<\/b>/,"")
-    air['items'][0]['subtitle'] = air['items'][0]['subtitle'].replace(/<b>/,"")
-    air['items'][0]['subtitle'] = air['items'][0]['subtitle'].replace(/<\/b>/,"")
+      movie['items'][0]['title'] = movie['items'][0]['title'].replace(/<b>/,"")
+      movie['items'][0]['title'] = movie['items'][0]['title'].replace(/<\/b>/,"")
+      movie['items'][0]['subtitle'] = movie['items'][0]['subtitle'].replace(/<b>/,"")
+      movie['items'][0]['subtitle'] = movie['items'][0]['subtitle'].replace(/<\/b>/,"")
     }
 
     if (error){
@@ -28,14 +28,14 @@ router.get('/', function(req,res,next){
     }
       return res.render('search', {
   
-      title: air['items'][0]['title'],
-      Director : air['items'][0]['director'],
-      Actor : air['items'][0]['actor'],
-      userRating : air['items'][0]['userRating'],
-      subtitle: air['items'][0]['subtitle'],
-      pubDate : air['items'][0]['pubDate'],
-      image : air['items'][0]['image'],
-      link : air['items'][0]['link'],
+      title: movie['items'][0]['title'],
+      Director : movie['items'][0]['director'],
+      Actor : movie['items'][0]['actor'],
+      userRating : movie['items'][0]['userRating'],
+      subtitle: movie['items'][0]['subtitle'],
+      pubDate : movie['items'][0]['pubDate'],
+      image : movie['items'][0]['image'],
+      link : movie['items'][0]['link'],
 
 
     })
